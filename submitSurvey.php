74bd5c1f8e6bf7b37467ec3870b597e69c08a93c
Jim McAndrew <jim@loc8.us>
2013-04-11 23:55:35 -0600

    Added the mysqli_real_escape_string function to prevent SQL injection

--- submitSurvey.php
+++ submitSurvey.php
@@ -6,20 +6,6 @@ $surveyAnswers = $_POST[answers];
 
 $answersArray = array();
 
-foreach ($surveyAnswers as $surveyAnswer) {
-    $valuesArray = array();
-    $counter = 0;
-    foreach ($surveyAnswer as $value) {
-        $valuesArray[] = $value;
-        if ($counter % 3 == 2 && count($valuesArray) > 0) {
-            $answersArray[] = $valuesArray;
-            $valuesArray = array();
-        }
-        $counter++;
-    }
-}
-
-
 // Open connection
 $con=mysqli_connect($db['server_name'],$db['user'],$db['password'],$db['database_name']);
 
@@ -29,7 +15,20 @@ if (mysqli_connect_errno($con))
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
   else {
-  
+
+    foreach ($surveyAnswers as $surveyAnswer) {
+        $valuesArray = array();
+        $counter = 0;
+        foreach ($surveyAnswer as $value) {
+            $valuesArray[] = mysqli_real_escape_string($con, $value);
+            if ($counter % 3 == 2 && count($valuesArray) > 0) {
+                $answersArray[] = $valuesArray;
+                $valuesArray = array();
+            }
+            $counter++;
+        }
+    }
+
     foreach ($answersArray as $values) {
         $sql = "INSERT INTO\n"
             . " simpleSurvey\n"
